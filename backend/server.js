55a3cb3f7515a89eab566e305c677b9d1cf21a2e
mukesh <mukeshcse0794@gmail.com>
2021-05-26 18:48:14 +0530

    error handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/config')
 
 const productRoutes = require('./routes/productsRoutes');
+const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', productRoutes);
+//custome define error handler
+app.use(errorHandler);
 
 app.listen(PORT, function () {
   console.log(`Server listening ${process.env.NODE_ENV} mode on port ${process.env.PORT}.`);
